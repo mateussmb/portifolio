e2c33676b589938d45b2396b23e7d0831fec834c
mateussmb <mt.moreirabarros@gmail.com>
2022-11-01 16:22:22 -0300

    portifolio

---
+++ script.js
@@ -0,0 +1,34 @@
+/*
+
+class MobileNavbar {
+    constructor(mobileMenu, navLinks, navList) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = 'active';
+
+    }
+
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", () => console.log("hey!"));
+    }
+
+    init () {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li"
+);
+mobileNavbar.init()
+
+   */
+   
